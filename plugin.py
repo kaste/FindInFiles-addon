19bff6371c8a301ff94aaa4824033d27f0830ea1
herr kaste <herr.kaste@gmail.com>
2022-08-05 22:40:07 +0200

    Enhance the standard Find in Files experience
    - Change the `result_line_regex` so it matches on every line
    - Add keybinding to jump between the different matches *without*
    switching to that location, just navigate within the Find in Files
    view.
    - Add keybinding `[enter]` to go to the location under the cursor.
    T.i. do the double-click think per keyboard.  (Note: works on all
    source code lines as we modified `result_line_regex`!)
    
    - Convenience, if there is a selection just do the "Find in Files"
    without further questions

--- plugin.py
+++ plugin.py
@@ -1,6 +1,8 @@
 import sublime
 import sublime_plugin
 
+from typing import List
+
 
 class fif_addon_refresh_last_search(sublime_plugin.TextCommand):
     """Delete last search result and do the search again
@@ -28,3 +30,43 @@ class fif_addon_refresh_last_search(sublime_plugin.TextCommand):
                 ["find_all"],
             ]
         })
+
+
+class fif_addon_listener(sublime_plugin.EventListener):
+    def is_applicable(self, view):
+        syntax = view.settings().get("syntax")
+        return syntax.endswith("Find Results.hidden-tmLanguage") if syntax else False
+
+    def on_activated_async(self, view):
+        if self.is_applicable(view):
+            view.settings().set("result_line_regex", "^ +([0-9]+)")
+
+
+class fif_addon_next_match(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        caret = view.sel()[0].b
+
+        for r in view.get_regions("match"):
+            if r.begin() > caret:
+                set_sel(view, [r])
+                view.show(r, False)
+                break
+
+
+class fif_addon_prev_match(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        caret = view.sel()[0].b
+
+        for r in reversed(view.get_regions("match")):
+            if r.end() < caret:
+                set_sel(view, [r])
+                view.show(r, False)
+                break
+
+
+def set_sel(view: sublime.View, selection: List[sublime.Region]) -> None:
+    sel = view.sel()
+    sel.clear()
+    sel.add_all(selection)
